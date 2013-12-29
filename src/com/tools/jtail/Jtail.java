5f39499f25787664720088b439cc41f2cfd50c95
Maarten van Leunen <maarten_l@yahoo.com>
2013-12-29 18:55:34 +0100

    implemented a first version of Jtail.

--- src/com/tools/jtail/Jtail.java
+++ src/com/tools/jtail/Jtail.java
@@ -16,19 +16,145 @@
  */
 package com.tools.jtail;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchService;
+import java.io.RandomAccessFile;
+import static java.nio.file.StandardWatchEventKinds.*;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
+ * JTail is my attempt at an implementation of the unix tail command in Java
+ * using the new NIO.2 classes. Main start class.
  *
  * @author maartenl
  */
 public class Jtail
 {
 
+    private static final Logger logger = Logger.getLogger(Jtail.class.getName());
+
+    private static long position = 0;
+
+    private static long size = 0;
+
+    @SuppressWarnings("unchecked")
+    private static <T> WatchEvent<T> cast(WatchEvent<?> event)
+    {
+        return (WatchEvent<T>) event;
+    }
+
     /**
      * @param args the command line arguments
+     * @throws IOException if something with wrong with the fileIO. For example,
+     * the file does not exist, or the watch service on the file borks.
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        // TODO code application logic here
+        ConsoleHandler handler = new ConsoleHandler();
+        final Level logLevel = Level.OFF; // Level.ALL;
+        handler.setLevel(logLevel);
+        logger.addHandler(handler);
+        logger.setLevel(logLevel);
+
+        logger.entering(Jtail.class.getName(), "main");
+        if (args.length != 1)
+        {
+            System.out.println("jtail <filename>");
+            System.exit(1);
+        }
+        String filename = args[0];
+        try (WatchService watcher = FileSystems.getDefault().newWatchService())
+        {
+            Path file = FileSystems.getDefault().getPath(filename);
+            Path directory = file.toAbsolutePath().getParent();
+            System.out.println("Get current path:" + FileSystems.getDefault().getPath(".").toAbsolutePath());
+            System.out.println("Directory:" + directory.toString());
+            // registering for file events
+            WatchKey key = directory.register(watcher, ENTRY_MODIFY);
+            // processing events
+            boolean keepGoing = true;
+            while (keepGoing)
+            {
+                WatchKey foundKey;
+                try
+                {
+                    foundKey = watcher.take();
+                } catch (InterruptedException ex)
+                {
+                    // waiting interrupted, lets' wait some more
+                    logger.log(Level.INFO, null, ex);
+                    continue;
+                }
+                List<WatchEvent<?>> events = foundKey.pollEvents();
+                // foundKey == key,... it's the only registered key
+                for (WatchEvent<?> event : events)
+                {
+                    processEvent(event, file);
+                }
+                keepGoing = foundKey.reset();
+            }
+            key.cancel();
+        }
+        logger.exiting(Jtail.class.getName(), "main");
     }
-    
+
+    private static void processEvent(WatchEvent<?> event, Path file) throws IOException
+    {
+        logger.entering(Jtail.class.getName(), "processEvent");
+        WatchEvent.Kind kind = event.kind();
+        // TBD - provide example of how OVERFLOW event is handled
+        if (kind == OVERFLOW)
+        {
+            return;
+        }
+        // Context for directory entry event is the file name of entry
+        WatchEvent<Path> ev = cast(event);
+        Path context = ev.context();
+        if (file.compareTo(context) == 0)
+        {
+            if (event.kind() == ENTRY_DELETE)
+            {
+                throw new IOException("File has been deleted.");
+            }
+            if (event.kind() == ENTRY_CREATE)
+            {
+                throw new IOException("File has been created. That's weird.");
+            }
+            // kind == ENTRY_MODIFY
+            tailFile(context);
+        }
+        logger.exiting(Jtail.class.getName(), "processEvent");
+    }
+
+    private static void tailFile(Path file) throws IOException
+    {
+        logger.entering(Jtail.class.getName(), "tailFile");
+        byte[] buffer = new byte[1024];
+        size = file.toFile().length();
+        if (position > size)
+        {
+            System.out.println("jtail: log.txt: file truncated");
+            position = 0;
+        }
+        try (RandomAccessFile reader = new RandomAccessFile(file.toFile(), "r");)
+        {
+            reader.seek(position);
+            while ((reader.read(buffer))
+                    != -1)
+            {
+                System.out.print(new String(buffer));
+            }
+            position = reader.getFilePointer();
+        }
+        size = position;
+        logger.exiting(Jtail.class.getName(), "tailFile");
+    }
+
 }
